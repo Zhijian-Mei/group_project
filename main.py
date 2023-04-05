6f2a0f26507850d86194903c87aecbe226b95425
Zhijian Mei <545902915@qq.com>
2023-04-05 22:36:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=0.001)
 max_length = 256
 trainSet = ToxicDataset(trainSet, tokenizer, max_length)
 evalSet = ToxicDataset(evalSet, tokenizer)
-train_batch_size = 20
-eval_batch_size = 8
+train_batch_size = 4
+eval_batch_size = 4
 train_loader = DataLoader(trainSet, batch_size=train_batch_size, shuffle=False)
 eval_loader = DataLoader(evalSet, batch_size=eval_batch_size)
 
