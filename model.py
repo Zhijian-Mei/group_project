3de7d4d523e6b266437f56423b05a974332cc91a
Zhijian Mei <545902915@qq.com>
2023-04-05 14:39:05 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,6 +5,7 @@ class RobertaMLP(nn.Module):
     def __init__(self,Roberta_model,config):
         super().__init__()
         self.model = Roberta_model
+        self.token_to_character = nn.Linear(512,2048)
         self.output = nn.ModuleList([
             nn.Linear(config.hidden_size,config.hidden_size*2),
             nn.Linear(config.hidden_size*2,2)
@@ -13,6 +14,7 @@ class RobertaMLP(nn.Module):
 
     def forward(self,text):
         x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
+        x = self.token_to_character(x)
         for module in self.output:
             x = module(x)
         return x
