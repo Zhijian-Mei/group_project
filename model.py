d05b514cf2d6ae2a1105d85071798de7e614df73
Zhijian Mei <545902915@qq.com>
2023-04-05 14:05:39 +0800

    1

--- model.py
+++ model.py
@@ -4,5 +4,10 @@ from torch import nn
 class RobertaMLP(nn.Module):
     def __init__(self,Roberta_model,config):
         super().init()
-        self.a = Roberta_model
-        self.b = nn.Linear(768,2048)
+        self.model = Roberta_model
+        self.output = nn.Linear(config['hidden_size'],2048)
+
+    def forward(self,text):
+        x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
+        output = self.output(x)
+        return output
