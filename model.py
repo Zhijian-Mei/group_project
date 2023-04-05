dd854b089bedc9a71b2ceef49304250907ed4c22
Zhijian Mei <545902915@qq.com>
2023-04-05 14:06:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -5,7 +5,7 @@ class RobertaMLP(nn.Module):
     def __init__(self,Roberta_model,config):
         super().__init__()
         self.model = Roberta_model
-        self.output = nn.Linear(config['hidden_size'],2048)
+        self.output = nn.Linear(config.hidden_size,2048)
 
     def forward(self,text):
         x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
