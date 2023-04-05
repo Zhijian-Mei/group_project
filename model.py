c769a4c47e13896f64eae4f2cf68ddbf8a65128a
Zhijian Mei <545902915@qq.com>
2023-04-05 15:34:33 +0800

    1

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class RobertaMLP(nn.Module):
             nn.Linear(config.hidden_size,config.hidden_size*2),
             nn.Linear(config.hidden_size*2,2)
                                     ])
-        self.sigmoid = nn.Sigmoid()
+
 
     def forward(self,text):
         x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
