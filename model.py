cff73629b258cd43695a1904b99d05d2d1f1c3dd
Zhijian Mei <545902915@qq.com>
2023-04-06 13:57:27 +0800

    1

--- model.py
+++ model.py
@@ -5,13 +5,15 @@ class RobertaMLP(nn.Module):
     def __init__(self,Roberta_model,config):
         super().__init__()
         self.model = Roberta_model
-
+        self.up = nn.Linear(256,1024)
         self.cls = nn.Linear(config.hidden_size,2)
 
 
     def forward(self,text):
         x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
-
+        x = torch.reshape(x,(x.shape[0],x.shape[2],x.shape[1]))
+        x = self.up(x)
+        x = torch.reshape(x,(x.shape[0],x.shape[2],x.shape[1]))
         x = self.cls(x)
 
         return x
