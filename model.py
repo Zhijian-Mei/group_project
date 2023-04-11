ec87864d32549433d4122042e3eec2d9a55b019f
Zhijian Mei <545902915@qq.com>
2023-04-12 00:35:04 +0800

    Update model.py

--- model.py
+++ model.py
@@ -30,14 +30,14 @@ class RobertaMLP_token(nn.Module):
         super().__init__()
         self.model = Roberta_model
         self.num_labels = 2
-        self.cls = nn.Linear(config.hidden_size,self.num_labels)
-
+        self.up = nn.Linear(config.hidden_size,2048)
+        self.down = nn.Linear(2048,self.num_labels)
 
 
     def forward(self,text,labels=None):
         x = self.model(text['input_ids'],text['attention_mask']).last_hidden_state
-        logits = self.cls(x)
-
+        x =  self.up(x)
+        logits = self.down(x)
 
         loss_fct = CrossEntropyLoss(ignore_index=-100)
 
